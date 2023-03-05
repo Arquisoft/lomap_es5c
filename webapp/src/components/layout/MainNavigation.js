5c676130be2e5310112a633097304c7b86c795fb
Jonathan Arias Busto <UO283586@uniovi.es>
2023-03-06 00:27:47 +0100

    fix about and home css for mobile devices

--- webapp/src/components/layout/MainNavigation.js
+++ webapp/src/components/layout/MainNavigation.js
@@ -3,6 +3,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import styles from "./MainNavigation.module.css";
+import LogInButton from "../LogInButton";
 
 const MainNavigation = () => {
   return (
@@ -19,6 +20,9 @@ const MainNavigation = () => {
             <NavLink to="/about" className={styles.active}>
               About
             </NavLink>
+          </li>
+          <li>
+            <LogInButton />
           </li>
         </ul>
       </nav>
