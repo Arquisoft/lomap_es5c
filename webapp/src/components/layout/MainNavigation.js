861ea3947de2e9ccc0c853585f478dc90166e43a
Laura Cordero <uo275955@uniovi.es>
2023-03-08 20:10:38 +0100

    Prueba logotipo app

--- webapp/src/components/layout/MainNavigation.js
+++ webapp/src/components/layout/MainNavigation.js
@@ -5,10 +5,20 @@ import { NavLink } from "react-router-dom";
 import styles from "./MainNavigation.module.css";
 import LogInButton from "../LogInButton";
 
+import logo from "../../images/LoMap_logo.png";
+
 const MainNavigation = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>LoMap</div>
+      <div className={styles.logo}>
+        <div className={styles.header_logo_image}>
+            <img
+              className={styles.header_img}
+              src={logo}
+              alt="LoMap Logo Image"
+            ></img>
+          </div>
+      </div>
       <nav className={styles.nav}>
         <ul>
           <li>
