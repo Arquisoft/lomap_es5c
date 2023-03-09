a37c5269525a2a19522929de5bef327f834e5ae6
gitblanc <UO285176@uniovi.es>
2023-03-09 10:49:40 +0100

    dependecies correction

--- webapp/src/components/Map/Maps.js
+++ webapp/src/components/Map/Maps.js
@@ -1,10 +1,10 @@
 import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  useMap,
-  // MapConsumer,
+	MapContainer,
+	TileLayer,
+	Marker,
+	Popup,
+	useMap,
+	// MapConsumer,
 } from "react-leaflet";
 import React from "react";
 import "leaflet/dist/leaflet.css";
@@ -12,42 +12,48 @@ import icon from "../../images/icon.png";
 import L from "leaflet";
 
 export default function Map({ coords, display_name }) {
-  const { latitude, longitude } = coords;
-  const markers = [[latitude, longitude]];
+	const { latitude, longitude } = coords;
+	const markers = [[latitude, longitude]];
 
-  const customIcon = new L.Icon({
-    iconUrl: icon,
-    iconSize: [25, 35],
-    iconAnchor: [5, 30],
-  });
+	const customIcon = new L.Icon({
+		iconUrl: icon,
+		iconSize: [25, 35],
+		iconAnchor: [5, 30],
+	});
 
-  const addMarker = (e) => {
-    console.log("TEST");
-    markers.push(e.latlng);
-    // this.setState({ markers });
-  };
+	const addMarker = (e) => {
+		console.log("TEST");
+		markers.push(e.latlng);
+		// this.setState({ markers });
+	};
 
-  function MapView() {
-    let map = useMap();
-    map.setView([latitude - 0.005, longitude], map.getZoom());
+	function MapView() {
+		let map = useMap();
+		// let map = useMapEvents({
+		// 	click(e) {
+		// 		markers.push(e.latlng);
+		// 		setMarkers((prevValue) => [...prevValue, e.latlng]);
+		// 	},
+		// });
+		map.setView([latitude - 0.005, longitude], map.getZoom());
 
-    return null;
-  }
+		return null;
+	}
 
-  return (
-    <MapContainer
-      classsName="map"
-      center={[latitude, longitude]}
-      zoom={15}
-      scrollWheelZoom={true}
-      click={addMarker}
-    >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> 
+	return (
+		<MapContainer
+			classsName="map"
+			center={[latitude, longitude]}
+			zoom={15}
+			scrollWheelZoom={true}
+			click={addMarker}
+		>
+			<TileLayer
+				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> 
         contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {/* <MapConsumer>
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+			{/* <MapConsumer>
         {(map) => {
           console.log("map center:", map.getCenter());
           map.on("click", function (e) {
@@ -57,10 +63,10 @@ export default function Map({ coords, display_name }) {
           return null;
         }}
       </MapConsumer> */}
-      <Marker icon={customIcon} position={[latitude, longitude]}>
-        <Popup>{display_name}</Popup>
-      </Marker>
-      <MapView />
-    </MapContainer>
-  );
+			<Marker icon={customIcon} position={[latitude, longitude]}>
+				<Popup>{display_name}</Popup>
+			</Marker>
+			<MapView />
+		</MapContainer>
+	);
 }
--- webapp/src/components/layout/MainNavigation.js
+++ webapp/src/components/layout/MainNavigation.js
@@ -4,42 +4,43 @@ import { NavLink } from "react-router-dom";
 
 import styles from "./MainNavigation.module.css";
 import LogInButton from "../LogInButton";
+//import LogInButton from "react-bootstrap/Button";
 
 import logo from "../../images/LoMap_logo.png";
 
 const MainNavigation = () => {
-  return (
-    <div className={styles.header_container}>
-      <header className={styles.header}>
-        <div className={styles.logo}>
-          <div className={styles.header_logo_image}>
-            <img
-              className={styles.header_img}
-              src={logo}
-              alt="LoMap Logo Image"
-            ></img>
-          </div>
-        </div>
-        <nav className={styles.nav}>
-          <ul>
-            <li>
-              <NavLink to="/home" className={styles.active}>
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/about" className={styles.active}>
-                About
-              </NavLink>
-            </li>
-            <li>
-              <LogInButton />
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </div>
-  );
+	return (
+		<div className={styles.header_container}>
+			<header className={styles.header}>
+				<div className={styles.logo}>
+					<div className={styles.header_logo_image}>
+						<img
+							className={styles.header_img}
+							src={logo}
+							alt="LoMap Logo Image"
+						></img>
+					</div>
+				</div>
+				<nav className={styles.nav}>
+					<ul>
+						<li>
+							<NavLink to="/home" className={styles.active}>
+								Home
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/about" className={styles.active}>
+								About
+							</NavLink>
+						</li>
+						<li>
+							<LogInButton />
+						</li>
+					</ul>
+				</nav>
+			</header>
+		</div>
+	);
 };
 
 export default MainNavigation;
