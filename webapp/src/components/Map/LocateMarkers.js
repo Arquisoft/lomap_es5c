764fac3c72e259f5d26134bc665b32b0854d8998
Jonathan Arias Busto <UO283586@uniovi.es>
2023-03-10 17:07:48 +0100

    possible rollback

--- webapp/src/components/Map/LocateMarkers.js
+++ webapp/src/components/Map/LocateMarkers.js
@@ -4,36 +4,83 @@ import { LatLng } from "leaflet";
 import icon from "../../images/icon.png";
 import L from "leaflet";
 
-function LocationMarkers({ coords, getPositionName }) {
-	const [markerName, setMarkerName] = useState();
-	const initialMarkers = [new LatLng(coords.latitude, coords.longitude)];
-	// const { latitude, longitude } = coords;
-	const [markers, setMarkers] = useState(initialMarkers);
+import InfoCard from "../UI/InfoCard";
 
-	const customIcon = new L.Icon({
-		iconUrl: icon,
-		iconSize: [25, 35],
-		iconAnchor: [5, 30],
-	});
+import styles from "./LocateMarkers.module.css";
 
-	const map = useMapEvents({
-		click(e) {
-			// setMarkerName(getPositionName(e.latlng.latitude, e.latlng.longitude));
-			console.log(e.latlng);
-			markers.push(e.latlng);
-			setMarkers((prevValue) => [...prevValue, e.latlng]);
-		},
-	});
+function LocationMarkers({ coords }) {
+  const [markerName, setMarkerName] = useState();
+  const initialMarkers = [new LatLng(coords.latitude, coords.longitude)];
+  // const { latitude, longitude } = coords;
+  const [markers, setMarkers] = useState(initialMarkers);
+  const [clicked, setClicked] = useState(false);
 
-	return (
-		<React.Fragment>
-			{markers.map((marker) => (
-				<Marker icon={customIcon} position={marker}>
-					<Popup>TEST</Popup>
-				</Marker>
-			))}
-		</React.Fragment>
-	);
+  const customIcon = new L.Icon({
+    iconUrl: icon,
+    iconSize: [25, 35],
+    iconAnchor: [5, 30],
+  });
+
+  async function getCurrentCityName(lat, long) {
+    let url =
+      "https://nominatim.openstreetmap.org/reverse?format=jsonv2" +
+      "&lat=" +
+      lat +
+      "&lon=" +
+      long;
+
+    console.log(url);
+
+    const response = await fetch(url, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Access-Control-Allow-Origin": "https://o2cj2q.csb.app",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setMarkerName(data.display_name));
+  }
+
+  function onClick(e) {
+    console.log(e);
+  }
+
+  const load = (
+    <div className={styles.info_container}>
+      <InfoCard position={markerName}></InfoCard>
+    </div>
+  );
+
+  const map = useMapEvents({
+    click(e) {
+      console.log("EJEMPLO");
+      // setMarkerName(getPositionName(e.latlng.latitude, e.latlng.longitude));
+      markers.push(e.latlng);
+      setMarkers((prevValue) => [...prevValue, e.latlng]);
+    },
+  });
+
+  return (
+    <React.Fragment>
+      {markers.map((marker, i) => (
+        <Marker
+          key={i}
+          icon={customIcon}
+          position={marker}
+          eventHandlers={{
+            click: (e) => {
+              setClicked(true);
+              getCurrentCityName(e.latlng.lat, e.latlng.lng);
+            },
+          }}
+        >
+          <Popup>TEST</Popup>
+        </Marker>
+      ))}
+      {clicked && load}
+    </React.Fragment>
+  );
 }
 
 export default LocationMarkers;
--- webapp/src/components/Map/Maps.js
+++ webapp/src/components/Map/Maps.js
@@ -1,10 +1,10 @@
 import {
-	MapContainer,
-	TileLayer,
-	Marker,
-	Popup,
-	useMap,
-	// MapConsumer,
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  // MapConsumer,
 } from "react-leaflet";
 import React from "react";
 import "leaflet/dist/leaflet.css";
@@ -12,47 +12,47 @@ import icon from "../../images/icon.png";
 import L from "leaflet";
 import LocationMarkers from "./LocateMarkers";
 
-export default function Map({ coords, display_name, getPositionName }) {
-	const { latitude, longitude } = coords;
-	const markers = [[latitude, longitude]];
+export default function Map({ coords }) {
+  const { latitude, longitude } = coords;
+  const markers = [[latitude, longitude]];
 
-	const customIcon = new L.Icon({
-		iconUrl: icon,
-		iconSize: [25, 35],
-		iconAnchor: [5, 30],
-	});
+  const customIcon = new L.Icon({
+    iconUrl: icon,
+    iconSize: [25, 35],
+    iconAnchor: [5, 30],
+  });
 
-	const addMarker = (e) => {
-		console.log("TEST");
-		markers.push(e.latlng);
-		// this.setState({ markers });
-	};
+  const addMarker = (e) => {
+    console.log("TEST");
+    markers.push(e.latlng);
+    // this.setState({ markers });
+  };
 
-	function MapView() {
-		let map = useMap();
-		map.setView([latitude - 0.005, longitude], map.getZoom());
+  function MapView() {
+    let map = useMap();
+    map.setView([latitude - 0.005, longitude], map.getZoom());
 
-		return null;
-	}
+    return null;
+  }
 
-	return (
-		<MapContainer
-			classsName="map"
-			center={[latitude, longitude]}
-			zoom={15}
-			scrollWheelZoom={true}
-			click={addMarker}
-		>
-			<TileLayer
-				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> 
+  return (
+    <MapContainer
+      classsName="map"
+      center={[latitude, longitude]}
+      zoom={15}
+      scrollWheelZoom={true}
+      click={addMarker}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> 
         contributors'
-				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-			/>
-			{/* <Marker icon={customIcon} position={[latitude, longitude]}>
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {/* <Marker icon={customIcon} position={[latitude, longitude]}>
 				<Popup>{display_name}</Popup>
 			</Marker> */}
-			<LocationMarkers coords={coords} getPositionName={getPositionName} />
-			<MapView />
-		</MapContainer>
-	);
+      <LocationMarkers coords={coords} />
+      <MapView />
+    </MapContainer>
+  );
 }
