bdbab9ff6b9bb5cc94e631afad74f821c2427faf
Jonathan Arias Busto <UO283586@uniovi.es>
2023-03-10 17:45:02 +0100

    update logo

--- webapp/src/components/layout/MainNavigation.js
+++ webapp/src/components/layout/MainNavigation.js
@@ -6,41 +6,44 @@ import styles from "./MainNavigation.module.css";
 import LogInButton from "../LogInButton";
 //import LogInButton from "react-bootstrap/Button";
 
-import logo from "../../images/LoMap_logo.png";
+// import logo from "../../images/LoMap_logo.png";
+// import logo from "../../images/test.png";
+import logo from "../../images/logo.png";
 
 const MainNavigation = () => {
-	return (
-		<div className={styles.header_container}>
-			<header className={styles.header}>
-				<div className={styles.logo}>
-					<div className={styles.header_logo_image}>
-						<img
-							className={styles.header_img}
-							src={logo}
-							alt="LoMap Logo Image"
-						></img>
-					</div>
-				</div>
-				<nav className={styles.nav}>
-					<ul>
-						<li>
-							<NavLink to="/home" className={styles.active}>
-								Home
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to="/about" className={styles.active}>
-								About
-							</NavLink>
-						</li>
-						<li>
-							<LogInButton />
-						</li>
-					</ul>
-				</nav>
-			</header>
-		</div>
-	);
+  return (
+    <div className={styles.header_container}>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <div className={styles.header_logo_image}>
+            <img
+              className={styles.header_img}
+              src={logo}
+              alt="LoMap Logo Image"
+            ></img>
+          </div>
+          <p>LoMap</p>
+        </div>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <NavLink to="/home" className={styles.active}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" className={styles.active}>
+                About
+              </NavLink>
+            </li>
+            <li>
+              <LogInButton />
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 };
 
 export default MainNavigation;
