d769a1fa9c1f68c1a5551ca93bfae0f652fdad7e
UO276967 <uo276967@uniovi.es>
2023-04-11 23:43:34 +0200

    Another test pass

--- webapp/src/tests/Pages/NotFound.test.js
+++ webapp/src/tests/Pages/NotFound.test.js
@@ -3,7 +3,7 @@ import NotFound from "../../components/Pages/NotFound";
 import i18n from "i18next";
 
 //It is in english by default
-test("The phrase saying that you need to log in to start is correct and by default in english", async () => {
+test("The phrase saying that the page is not found is correct and by default in english", async () => {
     i18n.changeLanguage("en");
     const {getByText} = render(<NotFound/>);
     expect(getByText('Page not found!')).toBeInTheDocument();
