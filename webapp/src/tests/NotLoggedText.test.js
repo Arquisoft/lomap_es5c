bfc62505cf73d0d6b78f8be1c946c0f1247feab7
UO276967 <uo276967@uniovi.es>
2023-04-11 16:18:57 +0200

    Changes in package.json for tests

---
+++ webapp/src/tests/NotLoggedText.test.js
@@ -0,0 +1,9 @@
+import {render} from "@testing-library/react";
+import NotLoggedText from "../src/components/UI/NotLoggedText";
+
+describe(NotLoggedText, () => {
+    it("The phrase saying that you need to log in to start is correct", () => {
+        const NotLoggedText = render(<NotLoggedText> </NotLoggedText>);
+        expect(NotLoggedText.container).toHaveTextContent('Not logged, please login!')
+    })
+})
