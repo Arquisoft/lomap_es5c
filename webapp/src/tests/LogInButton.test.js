b65e75ecb45986336a8025a86cee5246ab66d1cd
UO276967 <uo276967@uniovi.es>
2023-04-12 20:07:38 +0200

    Another test passed

--- webapp/src/tests/LogInButton.test.js
+++ webapp/src/tests/LogInButton.test.js
@@ -1,15 +1,20 @@
 import LogInButton from "../components/LogInButton";
-import {render, screen} from "@testing-library/react";
-
+import { screen} from "@testing-library/react";
 import React from 'react';
+import {render} from "../setupTests";
+import { I18nextProvider } from "react-i18next";
+import i18n from "i18next";
 
+//Por defecto es en inglés
 describe("LogInButton", () => {
-    test("The login view is rendered before pushing the button", async () => {
+    test("The login view is rendered before pushing the button and is in English by default", async () => {
+
         const {getByText}= render(<LogInButton isLoggedIn={false}/>);
         expect(getByText('LOGIN')).toBeInTheDocument();
     });
 
     test("The login view is rendered after pushing the button", async () => {
+ 
         const {getByText}= render(<LogInButton isLoggedIn={true}/>);
         expect(getByText('LOGOUT')).toBeInTheDocument();
     });
