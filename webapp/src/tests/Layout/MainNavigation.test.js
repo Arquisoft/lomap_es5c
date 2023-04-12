a3d1dcc2a299669eed73f193414b6dedc8fd92ae
UO277938 <uo277938@uniovi.es>
2023-04-12 16:55:15 +0200

    Added describe and LogInButton tests
    Now if in each test suite there are more than one test it has a describe, to identiy the component.

--- webapp/src/tests/Layout/MainNavigation.test.js
+++ webapp/src/tests/Layout/MainNavigation.test.js
@@ -3,5 +3,5 @@ import MainNavigation from "../../components/layout/MainNavigation";
 
 test("The main navigation bar is rendered", async () => {
     const {getByText} = render(<MainNavigation/>);
-    expect(getByText("LoMap")).toHaveInTheDocument();
+    expect(getByText("LoMap")).toBeInTheDocument();
 })
