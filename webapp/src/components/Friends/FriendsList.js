43399c80d702cc7dd974c5d6df7549f060eab1a4
Jonathan Arias Busto <UO283586@uniovi.es>
2023-04-18 12:50:13 +0200

    fix no markers loadingspinner sidemenu

--- webapp/src/components/Friends/FriendsList.js
+++ webapp/src/components/Friends/FriendsList.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import {
-	listFriends,
-	getFriendInfo,
-	deleteFriend,
+  listFriends,
+  getFriendInfo,
+  deleteFriend,
 } from "../Pods/PodsFunctions";
 import { useSession } from "@inrupt/solid-ui-react";
 import { useTranslation } from "react-i18next";
@@ -15,169 +15,171 @@ import { addNewFriend } from "../Pods/PodsFunctions";
 
 import defaultImage from "../../images/default_user.png";
 
-const FriendsList = ({ close, handleLoad }) => {
-	const { session } = useSession(); // Hook for providing access to the session in the component
-	const { webId } = session.info; // User's webId
+const FriendsList = ({ close, handleLoad, handleMarkersload }) => {
+  const { session } = useSession(); // Hook for providing access to the session in the component
+  const { webId } = session.info; // User's webId
 
-	const [friendsWebIds, setFriendsWebIds] = useState([]);
+  const [friendsWebIds, setFriendsWebIds] = useState([]);
 
-	let formIsValid = false;
+  let formIsValid = false;
 
-	const {
-		value: enteredFriendWebId,
-		isValid: validFriendWebId,
-		hasError: friendWebIdInputHasError,
-		valueChangeHandler: friendWebIdChangeHandler,
-		inputBlurHandler: friendWebIdBlurHandler,
-		reset: resetFriendWebIdInput,
-	} = useInput((value) => value.trim() !== "");
+  const {
+    value: enteredFriendWebId,
+    isValid: validFriendWebId,
+    hasError: friendWebIdInputHasError,
+    valueChangeHandler: friendWebIdChangeHandler,
+    inputBlurHandler: friendWebIdBlurHandler,
+    reset: resetFriendWebIdInput,
+  } = useInput((value) => value.trim() !== "");
 
-	const getFriendsWebIds = async () => {
-		setFriendsWebIds([]);
+  const getFriendsWebIds = async () => {
+    setFriendsWebIds([]);
 
-		var auxFriends = await listFriends(webId);
-		auxFriends.map(async (friend) => {
-			var friendData = await getFriendInfo(friend, session);
-			setFriendsWebIds((prevValue) => [
-				...prevValue,
-				{
-					friend,
-					friendData,
-				},
-			]);
-		});
+    var auxFriends = await listFriends(webId);
+    auxFriends.map(async (friend) => {
+      var friendData = await getFriendInfo(friend, session);
+      setFriendsWebIds((prevValue) => [
+        ...prevValue,
+        {
+          friend,
+          friendData,
+        },
+      ]);
+    });
 
-		handleLoad(true);
-	};
+    handleLoad(true);
+  };
 
-	const handleDeleteFriend = async (friendWebId) => {
-		await deleteFriend(webId, session, friendWebId);
-		getFriendsWebIds();
-	};
+  const handleDeleteFriend = async (friendWebId) => {
+    await deleteFriend(webId, session, friendWebId);
+    getFriendsWebIds();
+    handleMarkersload(false);
+  };
 
-	if (validFriendWebId) formIsValid = true;
+  if (validFriendWebId) formIsValid = true;
 
-	const addFriendHandler = async (event) => {
-		event.preventDefault();
-		// Check if the input is valid
-		if (!validFriendWebId) {
-			return;
-		}
+  const addFriendHandler = async (event) => {
+    event.preventDefault();
+    // Check if the input is valid
+    if (!validFriendWebId) {
+      return;
+    }
 
-		handleLoad(false);
+    handleLoad(false);
+    handleMarkersload(false);
 
-		// Add the new friend
-		var exit = await addNewFriend(webId, session, enteredFriendWebId);
+    // Add the new friend
+    var exit = await addNewFriend(webId, session, enteredFriendWebId);
 
-		if (exit) {
-			getFriendsWebIds();
-		} else {
-			alert("Friend not added");
-		}
-		// Reset the input
-		resetFriendWebIdInput();
-	};
+    if (exit) {
+      getFriendsWebIds();
+    } else {
+      alert("Friend not added");
+    }
+    // Reset the input
+    resetFriendWebIdInput();
+  };
 
-	useEffect(() => {
-		getFriendsWebIds();
-	}, []);
+  useEffect(() => {
+    getFriendsWebIds();
+  }, []);
 
-	const [t, i18n] = useTranslation("translation");
+  const [t, i18n] = useTranslation("translation");
 
-	let headerStyle =
-		window.localStorage.getItem("themeStyle") === "dark" ? "#fff " : "#000 ";
+  let headerStyle =
+    window.localStorage.getItem("themeStyle") === "dark" ? "#fff " : "#000 ";
 
-	return (
-		<>
-			<div className="d-flex mx-2 justify-content-center">
-				<h3 style={{ color: headerStyle }}>{t("FriendsList.add")}</h3>
-			</div>
-			<div className="my-2 mx-2" style={{ width: "95%" }}>
-				<div className="card my-2">
-					<div className="form-group mx-2 my-2">
-						<form onSubmit={addFriendHandler}>
-							<label htmlFor="exampleInputEmail1">
-								{t("FriendsList.enter")}
-							</label>
-							<input
-								type="text"
-								className="form-control"
-								id="inputFriendWebId"
-								aria-describedby="friendWebIdHelp"
-								placeholder="webId"
-								onChange={friendWebIdChangeHandler}
-							/>
-							<Button
-								type="submit"
-								className={
-									formIsValid ? "btn-btn-primary" : "btn btn-secondary"
-								}
-								disabled={!formIsValid}
-							>
-								{t("FriendsList.submit")}
-							</Button>
-						</form>
-					</div>
-				</div>
-			</div>
-			<div className="d-flex mx-2 justify-content-center">
-				<h3 style={{ color: headerStyle }}>{t("FriendsList.list")}</h3>
-			</div>
-			<div className="my-2 mx-2" style={{ width: "95%" }}>
-				{friendsWebIds.map((friend, i) => {
-					return (
-						<div key={i} className="card mb-2">
-							<div className="d-flex mx-2 my-2 align-items-center">
-								{friend.friendData.imageUrl != null ||
-								friend.friendData.imageUrl != undefined ? (
-									<img
-										src={friend.friendData.imageUrl}
-										style={{
-											width: "90px",
-											height: "90px",
-											borderRadius: "6em",
-										}}
-									></img>
-								) : (
-									<img
-										src={defaultImage}
-										style={{
-											width: "90px",
-											height: "90px",
-											borderRadius: "6em",
-										}}
-									></img>
-								)}
-								<div className="d-flex justify-content-start mx-2 mb-2 ">
-									<p
-										className="card-title"
-										style={{ fontSize: "1.175rem", marginBottom: 0 }}
-									>
-										{friend.friendData.name}
-									</p>
-								</div>
-								<div className="d-flex w-100 align-items-center justify-content-end mx-2">
-									<button
-										className="btn btn-danger btn-sm rounded-10"
-										type="button"
-										data-toggle="tooltip"
-										data-placement="top"
-										title="Delete"
-										style={{ minWidth: "40px", minHeight: "40px" }}
-										onClick={() => {
-											handleDeleteFriend(friend.friend);
-										}}
-									>
-										<i className="fa fa-trash" style={{ fontSize: "20px" }}></i>
-									</button>{" "}
-								</div>
-							</div>
-						</div>
-					);
-				})}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="d-flex mx-2 justify-content-center">
+        <h3 style={{ color: headerStyle }}>{t("FriendsList.add")}</h3>
+      </div>
+      <div className="my-2 mx-2" style={{ width: "95%" }}>
+        <div className="card my-2">
+          <div className="form-group mx-2 my-2">
+            <form onSubmit={addFriendHandler}>
+              <label htmlFor="exampleInputEmail1">
+                {t("FriendsList.enter")}
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputFriendWebId"
+                aria-describedby="friendWebIdHelp"
+                placeholder="webId"
+                onChange={friendWebIdChangeHandler}
+              />
+              <Button
+                type="submit"
+                className={
+                  formIsValid ? "btn-btn-primary" : "btn btn-secondary"
+                }
+                disabled={!formIsValid}
+              >
+                {t("FriendsList.submit")}
+              </Button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="d-flex mx-2 justify-content-center">
+        <h3 style={{ color: headerStyle }}>{t("FriendsList.list")}</h3>
+      </div>
+      <div className="my-2 mx-2" style={{ width: "95%" }}>
+        {friendsWebIds.map((friend, i) => {
+          return (
+            <div key={i} className="card mb-2">
+              <div className="d-flex mx-2 my-2 align-items-center">
+                {friend.friendData.imageUrl != null ||
+                friend.friendData.imageUrl != undefined ? (
+                  <img
+                    src={friend.friendData.imageUrl}
+                    style={{
+                      width: "90px",
+                      height: "90px",
+                      borderRadius: "6em",
+                    }}
+                  ></img>
+                ) : (
+                  <img
+                    src={defaultImage}
+                    style={{
+                      width: "90px",
+                      height: "90px",
+                      borderRadius: "6em",
+                    }}
+                  ></img>
+                )}
+                <div className="d-flex justify-content-start mx-2 mb-2 ">
+                  <p
+                    className="card-title"
+                    style={{ fontSize: "1.175rem", marginBottom: 0 }}
+                  >
+                    {friend.friendData.name}
+                  </p>
+                </div>
+                <div className="d-flex w-100 align-items-center justify-content-end mx-2">
+                  <button
+                    className="btn btn-danger btn-sm rounded-10"
+                    type="button"
+                    data-toggle="tooltip"
+                    data-placement="top"
+                    title="Delete"
+                    style={{ minWidth: "40px", minHeight: "40px" }}
+                    onClick={() => {
+                      handleDeleteFriend(friend.friend);
+                    }}
+                  >
+                    <i className="fa fa-trash" style={{ fontSize: "20px" }}></i>
+                  </button>{" "}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default FriendsList;
--- webapp/src/components/layout/SideMenu.js
+++ webapp/src/components/layout/SideMenu.js
@@ -19,210 +19,233 @@ import OptionsMenu from "./OptionsMenu";
 import FilterCard from "./FilterCard";
 
 const SideMenu = ({ option, prevOption, coords, handleOption }) => {
-	const ctx = useContext(UserSessionContext);
+  const ctx = useContext(UserSessionContext);
 
-	const [loaded, setLoaded] = React.useState(false);
-	const [loadedUserPods, setLoadedUserPods] = React.useState(false);
-	const [loadedFriends, setLoadedFriends] = React.useState(false);
+  const [loaded, setLoaded] = React.useState(false);
+  const [loadedUserPods, setLoadedUserPods] = React.useState(false);
+  const [loadedFriends, setLoadedFriends] = React.useState(false);
 
-	const { session } = useSession(); // Hook for providing access to the session in the component
-	const { webId } = session.info; // User's webId
+  const { session } = useSession(); // Hook for providing access to the session in the component
+  const { webId } = session.info; // User's webId
 
-	const [firstLoad, setFirstLoad] = React.useState(true);
+  const [firstLoad, setFirstLoad] = React.useState(true);
 
-	const [updatePoints, setUpdatePoints] = React.useState(false);
+  const [updatePoints, setUpdatePoints] = React.useState(false);
 
-	const [closed, setClosed] = React.useState(false);
+  const [closed, setClosed] = React.useState(false);
 
-	const loadUserPodsMarkers = async () => {
-		setClosed(false);
-		setLoaded(false);
-		ctx.handleLoaded(false);
-		ctx.handleFilterOption("all");
+  const loadUserPodsMarkers = async () => {
+    setClosed(false);
+    setLoaded(false);
+    ctx.handleLoaded(false);
+    ctx.handleFilterOption("all");
 
-		var locations = [];
+    var locations = [];
 
-		locations.push(await listLocationsOfAUser(webId, session));
+    locations.push(await listLocationsOfAUser(webId, session));
 
-		ctx.handleMarkers(locations);
-		ctx.handleFilteredMarkers([]); // we add the markers to the context
-		setLoadedUserPods(true);
-		setFirstLoad(false);
-		setUpdatePoints(false);
-	};
+    ctx.handleMarkers(locations);
+    ctx.handleFilteredMarkers([]); // we add the markers to the context
+    setLoadedUserPods(true);
+    setFirstLoad(false);
+    setUpdatePoints(false);
+  };
 
-	const loadPodsMarkers = async () => {
-		setClosed(false);
-		setLoaded(false);
-		ctx.handleLoaded(false);
-		ctx.handleFilterOption("all");
-		var usersIds = await listFriends(webId);
-		// usersIds.push(webId); uncomment if u want to also load your own markers
-		var locations = [];
-		for (let i = 0; i < usersIds.length; i++) {
-			let aux = await listLocationsOfAUser(usersIds[i], session);
-			if (aux !== null) {
-				locations.push(aux);
-			}
-		}
+  const loadPodsMarkers = async () => {
+    setClosed(false);
+    setLoaded(false);
+    ctx.handleLoaded(false);
+    ctx.handleFilterOption("all");
+    var usersIds = await listFriends(webId);
+    // usersIds.push(webId); uncomment if u want to also load your own markers
+    var locations = [];
+    for (let i = 0; i < usersIds.length; i++) {
+      let aux = await listLocationsOfAUser(usersIds[i], session);
+      if (aux !== null) {
+        locations.push(aux);
+      }
+    }
 
-		setLoadedUserPods(false);
-		ctx.handleMarkers(locations);
-		ctx.handleFilteredMarkers([]);
-		setLoaded(true);
-		setUpdatePoints(false);
-	};
+    setLoadedUserPods(false);
+    ctx.handleMarkers(locations);
+    ctx.handleFilteredMarkers([]);
+    setLoaded(true);
+    setUpdatePoints(false);
+  };
 
-	const handleClick = () => {
-		setFirstLoad(!firstLoad);
-	};
+  const handleClick = () => {
+    setFirstLoad(!firstLoad);
+  };
 
-	useEffect(() => {
-		if (firstLoad) {
-			loadUserPodsMarkers();
-		}
-	}, []);
+  useEffect(() => {
+    if (firstLoad) {
+      loadUserPodsMarkers();
+    }
+  }, []);
 
-	useEffect(() => {
-		if (option === "read" && !loaded) {
-			setLoaded(false);
-			loadPodsMarkers();
-		} else if (option === "userPods" && !firstLoad && !loadedUserPods) {
-			loadUserPodsMarkers();
-		}
-	}, [option]);
+  useEffect(() => {
+    if (option === "read" && !loaded) {
+      setLoaded(false);
+      loadPodsMarkers();
+    } else if (option === "userPods" && !firstLoad && !loadedUserPods) {
+      loadUserPodsMarkers();
+    }
+  }, [option]);
 
-	useEffect(() => {
-		if (updatePoints) {
-			loadUserPodsMarkers();
-		}
-	}, [updatePoints]);
+  useEffect(() => {
+    if (updatePoints) {
+      loadUserPodsMarkers();
+    }
+  }, [updatePoints]);
 
-	useEffect(() => {
-		if (ctx.selectedMarker !== null) {
-			handleOption("markerInfo");
-		}
-	}, [ctx.selectedMarker]);
+  useEffect(() => {
+    if (ctx.selectedMarker !== null) {
+      handleOption("markerInfo");
+    }
+  }, [ctx.selectedMarker]);
 
-	let styleButton =
-		ctx.pageStyle === "light"
-			? "btn-close mx-3 mt-2"
-			: "btn-close mx-3 mt-2 btn-close-white";
+  let styleButton =
+    ctx.pageStyle === "light"
+      ? "btn-close mx-3 mt-2"
+      : "btn-close mx-3 mt-2 btn-close-white";
 
-	return (
-		<>
-			<OptionsMenu
-				changeOption={(opt) => {
-					handleOption(opt);
-				}}
-			/>
-			{option === "userPods" && !loadedUserPods && (
-				<div className="d-flex justify-content-center align-items-center h-100">
-					<LoadingSpinner />
-				</div>
-			)}
-			{loadedUserPods &&
-				ctx.filteredMarkers.length === 0 &&
-				option === "userPods" &&
-				ctx.markers.map((marker, i) => {
-					return <MarkerCard key={i} marker={marker} />;
-				})}
-			{option === "userPods" &&
-				ctx.filteredMarkers.length > 0 &&
-				ctx.filteredMarkers.map((marker, i) => {
-					return <MarkerCard key={i} marker={marker} />;
-				})}
+  let headerStyle =
+    window.localStorage.getItem("themeStyle") === "dark" ? "#fff " : "#000 ";
 
-			{option === "create" && (
-				<PodCreateForm
-					coords={coords}
-					close={handleOption}
-					prevOption={prevOption}
-					needsUpdate={setUpdatePoints}
-				/>
-			)}
-			{option === "read" && !loaded && (
-				<div className="d-flex justify-content-center align-items-center h-100">
-					<LoadingSpinner />
-				</div>
-			)}
-			{option === "read" &&
-				loaded &&
-				ctx.markers.map((marker, i) => {
-					return <MarkerCard key={i} marker={marker} />;
-				})}
-			{option === "friends" && (
-				<>
-					<div className="d-flex justify-content-end">
-						<button
-							type="button"
-							// className="btn-close mx-3 mt-2"
-							className={styleButton}
-							style={{ fontSize: "1rem" }}
-							aria-label="Close"
-							onClick={() => {
-								// handleOption("userPods");
-								handleOption(prevOption);
-								setClosed(true);
-								ctx.handleSelectedMarker(null);
-							}}
-						></button>
-					</div>
-					<FriendsList
-						close={handleOption}
-						handleLoad={(opt) => {
-							setLoadedFriends(opt);
-						}}
-					/>
-				</>
-			)}
-			{option === "friends" && !loadedFriends && (
-				<div className="d-flex justify-content-center align-items-center h-100">
-					<LoadingSpinner />
-				</div>
-			)}
-			{option === "markerInfo" && (
-				<>
-					<div className="d-flex justify-content-end">
-						<button
-							type="button"
-							// className="btn-close mx-3 mt-2"
-							className={styleButton}
-							style={{ fontSize: "1rem" }}
-							aria-label="Close"
-							onClick={() => {
-								setClosed(true); // Search for memory leaks here MaxListenersExceededWarning: Possible EventEmitter memory leak detected. 11 login listeners added.
-								// handleOption("userPods");
-								handleOption(prevOption);
-								ctx.handleSelectedMarker(null);
-							}}
-						></button>
-					</div>
-					<MarkerCard marker={ctx.selectedMarker} />
-				</>
-			)}
-			{option === "filter" && (
-				<>
-					<div className="d-flex justify-content-end my-2">
-						<button
-							type="button"
-							// className="btn-close mx-3 mt-2"
-							className={styleButton}
-							style={{ fontSize: "1rem" }}
-							aria-label="Close"
-							onClick={() => {
-								setClosed(true);
-								// handleOption("userPods");
-								handleOption(prevOption);
-								ctx.handleSelectedMarker(null);
-							}}
-						></button>
-					</div>
-					<FilterCard />
-				</>
-			)}
-		</>
-	);
+  return (
+    <>
+      <OptionsMenu
+        changeOption={(opt) => {
+          handleOption(opt);
+        }}
+      />
+      {option === "userPods" && !loadedUserPods && (
+        <div className="d-flex justify-content-center align-items-center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {option === "userPods" && ctx.markers.length === 0 && loadedUserPods && (
+        <div
+          className="d-flex justify-content-center align-items-center"
+          style={{ height: "auto" }}
+        >
+          <h3 style={{ color: headerStyle }}>NO MARKERS</h3>
+        </div>
+      )}
+      {loadedUserPods &&
+        ctx.filteredMarkers.length === 0 &&
+        option === "userPods" &&
+        ctx.markers.map((marker, i) => {
+          return <MarkerCard key={i} marker={marker} />;
+        })}
+      {option === "userPods" &&
+        ctx.filteredMarkers.length > 0 &&
+        ctx.filteredMarkers.map((marker, i) => {
+          return <MarkerCard key={i} marker={marker} />;
+        })}
+
+      {option === "create" && (
+        <PodCreateForm
+          coords={coords}
+          close={handleOption}
+          prevOption={prevOption}
+          needsUpdate={setUpdatePoints}
+        />
+      )}
+
+      {option === "read" && loaded && ctx.markers.length === 0 && (
+        <div
+          className="d-flex justify-content-center align-items-center"
+          style={{ height: "auto" }}
+        >
+          <h3 style={{ color: headerStyle }}>NO MARKERS</h3>
+        </div>
+      )}
+      {option === "read" && !loaded && (
+        <div className="d-flex justify-content-center align-items-center h-100">
+          <LoadingSpinner />
+        </div>
+      )}
+      {option === "read" &&
+        loaded &&
+        ctx.markers.map((marker, i) => {
+          return <MarkerCard key={i} marker={marker} />;
+        })}
+      {option === "friends" && (
+        <>
+          <div className="d-flex justify-content-end">
+            <button
+              type="button"
+              // className="btn-close mx-3 mt-2"
+              className={styleButton}
+              style={{ fontSize: "1rem" }}
+              aria-label="Close"
+              onClick={() => {
+                // handleOption("userPods");
+                handleOption(prevOption);
+                setClosed(true);
+                ctx.handleSelectedMarker(null);
+              }}
+            ></button>
+          </div>
+          <FriendsList
+            close={handleOption}
+            handleLoad={(opt) => {
+              setLoadedFriends(opt);
+            }}
+            handleMarkersload={(opt) => {
+              setLoaded(opt);
+            }}
+          />
+        </>
+      )}
+      {option === "friends" && !loadedFriends && (
+        <div className="d-flex justify-content-center align-items-center h-100">
+          <LoadingSpinner />
+        </div>
+      )}
+      {option === "markerInfo" && (
+        <>
+          <div className="d-flex justify-content-end">
+            <button
+              type="button"
+              // className="btn-close mx-3 mt-2"
+              className={styleButton}
+              style={{ fontSize: "1rem" }}
+              aria-label="Close"
+              onClick={() => {
+                setClosed(true); // Search for memory leaks here MaxListenersExceededWarning: Possible EventEmitter memory leak detected. 11 login listeners added.
+                // handleOption("userPods");
+                handleOption(prevOption);
+                ctx.handleSelectedMarker(null);
+              }}
+            ></button>
+          </div>
+          <MarkerCard marker={ctx.selectedMarker} />
+        </>
+      )}
+      {option === "filter" && (
+        <>
+          <div className="d-flex justify-content-end my-2">
+            <button
+              type="button"
+              // className="btn-close mx-3 mt-2"
+              className={styleButton}
+              style={{ fontSize: "1rem" }}
+              aria-label="Close"
+              onClick={() => {
+                setClosed(true);
+                // handleOption("userPods");
+                handleOption(prevOption);
+                ctx.handleSelectedMarker(null);
+              }}
+            ></button>
+          </div>
+          <FilterCard />
+        </>
+      )}
+    </>
+  );
 };
 
 export default SideMenu;
