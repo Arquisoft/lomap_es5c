74db462cfebb2c17b9bfdc876d2aa1201c7d1b08
UO277938 <uo277938@uniovi.es>
2023-04-24 14:56:03 +0200

    FilterButton test
    And some changes in SideMenu

--- webapp/src/tests/Layout/SideMenu.test.js
+++ webapp/src/tests/Layout/SideMenu.test.js
@@ -10,13 +10,21 @@ jest.mock("@inrupt/solid-ui-react", () => ({
 	useSession: () => ({
 		session: {
 			info: {
-				webId: "https://iimxinnn.inrupt.net/profile/card#me",
+				webId: "https://uo277938.inrupt.net/profile/card#me",
 			},
 		},
 	}),
 }));
 
 describe("SideMenu", () => {
+	test('renders the sideMenu correctly', () => {
+		const { getByText, container } = render(<SideMenu option="userPods" />);
+		expect(getByText("Markers")).toBeInTheDocument();
+		expect(getByText("Friends Markers")).toBeInTheDocument();
+		expect(getByText("Friends")).toBeInTheDocument();
+		expect(getByText("Filter")).toBeInTheDocument();
+	});
+
 	test('renders loading spinner when option is "userPods" and markers are not loaded yet', () => {
 		const { container } = render(<SideMenu option="userPods" />);
 		const element = container.getElementsByClassName("spinner")[0];
@@ -31,8 +39,17 @@ describe("SideMenu", () => {
 	});
 
 	test('renders PodCreateForm component when option is "create"', () => {
-		const { getByText } = render(<SideMenu option="create" />);
+		const { getByText, getAllByRole } = render(<SideMenu option="create" />);
 		expect(getByText("Create location")).toBeInTheDocument();
+		expect(getByText("Title")).toBeInTheDocument();
+		expect(getByText("Description")).toBeInTheDocument();
+		expect(getByText("Category")).toBeInTheDocument();
+		expect(getByText("Create")).toBeInTheDocument();
+
+		const options = getAllByRole("option");
+    	expect(options[0].textContent).toBe("Other");
+    	expect(options.length).toBe(18);
+
 	});
 
 	test('renders no markers message when option is "read" and no markers are available', async () => {
@@ -43,18 +60,14 @@ describe("SideMenu", () => {
 
 		//await waitFor(() => expect(screen.getByText('No markers found')).toBeInTheDocument());
 	});
-	/*
-  test("loads markers of friends when 'read' option is selected", async () => {
-    const { findByTestId, container } = render(<SideMenu />);
-    container = container.querySelectorAll(".read")[0];
+	
+  /*test("loads markers of friends when 'read' option is selected", async () => {
+    const { findByTestId, getByText } = render(<SideMenu />);
+    const friendsMarkers = getByText("Friends Markers");
 
-    fireEvent.click(container);
+    fireEvent.click(friendsMarkers);
 
-    const loadingElement = await findByTestId("loading-spinner");
-    expect(loadingElement).toBeInTheDocument();
-
-    const markerCardElements = await findByTestId("marker-card");
-    expect(markerCardElements.length).toBeGreaterThan(0);
+    //await waitFor(() => expect(getByText("Punto1")).toBeInTheDocument())
   });*/
 
 	/*test("The side menu is rendered in Engish by default and it should set loadedUserPods to true", async() => {
