6517f9a1fa23b86ed63e7cde9a3bcffad199fd6a
UO277938 <uo277938@uniovi.es>
2023-04-26 21:33:03 +0200

    OptionsMenu tests

--- webapp/src/tests/Layout/OptionsMenu.test.js
+++ webapp/src/tests/Layout/OptionsMenu.test.js
@@ -1,40 +1,60 @@
 import OptionsMenu from "../../components/layout/OptionsMenu";
 import {render} from "../../setupTests";
 import {screen, fireEvent, waitFor} from '@testing-library/react';
+import UserSessionContext from '../../store/session-context';
 import i18n from "i18next";
 
 //Por defecto es en inglés
 
 describe("OptionsMenu", () => {
-    test("The Options Menu is rendered and it is in english by default", async() => {
-        const { getByText } = render(<OptionsMenu />);
-        const buttons = screen.getAllByRole('button');
+  const changeOptionMock = jest.fn();
+  const contextValues = { loaded: true };
+  const setup = () => {
+    return render(
+      <UserSessionContext.Provider value={contextValues}>
+        <OptionsMenu changeOption={changeOptionMock} />
+      </UserSessionContext.Provider>
+    );
+  };
 
-        expect(getByText("Markers")).toBeInTheDocument();
-        expect(getByText("Friends Markers")).toBeInTheDocument();
-        expect(getByText("Friends")).toBeInTheDocument();
-        expect(getByText("Filter")).toBeInTheDocument();
-        expect(buttons.length).toBe(4);
-    })
+  test('renders without errors', () => {
+    setup();
+  });
 
+  test("The Options Menu is rendered and it is in english by default", () => {
+      const { getByText } = render(<OptionsMenu />);
+      const buttons = screen.getAllByRole('button');
 
-    test("should render four buttons with the correct text by default in english", () => {
-        const { getByText } = render(<OptionsMenu />);
-    
-        expect(getByText("Markers")).toBeInTheDocument();
-        expect(getByText("Friends Markers")).toBeInTheDocument();
-        expect(getByText("Friends")).toBeInTheDocument();
-        expect(getByText("Filter")).toBeInTheDocument();
-      });
+      expect(getByText("Markers")).toBeInTheDocument();
+      expect(getByText("Friends Markers")).toBeInTheDocument();
+      expect(getByText("Friends")).toBeInTheDocument(); 
+      expect(getByText("Filter")).toBeInTheDocument();
+      expect(buttons.length).toBe(4);
+  });
 
+  test('disables buttons when context is not loaded', () => {
+    contextValues.loaded = false;
+    setup();
+    expect(screen.getByText("Markers")).toBeDisabled();
+    expect(screen.getByText("Friends Markers")).toBeDisabled();
+    expect(screen.getByText("Filter")).toBeDisabled();
+  });
 
-      /* test("should call changeOption with correct value when a button is clicked/is pushed correctly", () => {
-        const changeOptionMock = jest.fn();
-        const { getByText } = render(<OptionsMenu changeOption={changeOptionMock} />);
-    
-        fireEvent.click(getByText("Markers"));
-    
-        expect(changeOptionMock).toHaveBeenCalledWith("userPods");
-      }); */
+  test('calls changeOption with the correct option when a button is clicked', () => {
+    contextValues.loaded = true;
+    const { getByTestId } = setup();
 
-})
+    fireEvent.click(getByTestId("userPods"));
+    expect(changeOptionMock).toHaveBeenCalledWith('userPods');
+
+    fireEvent.click(getByTestId("read"));
+    expect(changeOptionMock).toHaveBeenCalledWith('read');
+
+    fireEvent.click(getByTestId("friends")); 
+    expect(changeOptionMock).toHaveBeenCalledWith('friends');
+
+    fireEvent.click(getByTestId("filter"));
+    expect(changeOptionMock).toHaveBeenCalledWith('filter');
+  });
+
+});
